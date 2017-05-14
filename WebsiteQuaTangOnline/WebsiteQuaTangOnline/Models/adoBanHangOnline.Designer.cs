e75f6af86338b79d747b634807350c1ad4cbb7c0
Hoàng Trung Lực <Hong Trung Lc>
2017-05-14 17:07:02 +0700

    14/5/2017

--- WebsiteQuaTangOnline/WebsiteQuaTangOnline/Models/adoBanHangOnline.Designer.cs
+++ WebsiteQuaTangOnline/WebsiteQuaTangOnline/Models/adoBanHangOnline.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\HocTap\HK2-2016-2017\SE109\Git\WebSiteBanHang\WebsiteQuaTangOnline\WebsiteQuaTangOnline\Models\adoBanHangOnline.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\SE109\WebSiteBanHang\WebsiteQuaTangOnline\WebsiteQuaTangOnline\Models\adoBanHangOnline.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
